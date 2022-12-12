3d76c5024dfd4cea0340bc74325c5b372ac13dbc
kumarmudur <kumarmudur10@gmail.com>
2022-12-12 20:52:42 +0530

    [React-Hooks][useContext] use context example

---
+++ src/components/hooks/context/ComponentC.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ComponentE from './ComponentE';
+
+const componentC = () => {
+    return (
+        <div>
+            <ComponentE />
+        </div>
+    );
+};
+
+export default componentC;
