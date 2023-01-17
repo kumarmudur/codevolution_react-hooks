19c8470014619be3e69b34036c4b9fabad67b822
kumarmudur <kumarmudur10@gmail.com>
2023-01-17 08:35:32 +0530

    [React-Hooks] app component changes

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function App() {
       </ChannelContext.Provider>
      </UserContext.Provider> */}
 
-     {/* <CounterOne /> */}
+     <CounterOne />
      {/* <CounterTwo /> */}
      {/* <CounterThree /> */}
 
